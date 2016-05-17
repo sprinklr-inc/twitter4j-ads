5dfc4fcc3ec1b0c392178f02468b3216ea3ba2c8
abhishek anand <aanand@sprinklr.com>
2016-05-17 16:32:03 +0530

    removed keys for Auth

--- twitter4j-ads-examples/src/examples/BaseAdsTest.java
+++ twitter4j-ads-examples/src/examples/BaseAdsTest.java
@@ -12,8 +12,7 @@ public class BaseAdsTest {
 
     public static TwitterAds getTwitterAdsInstance() {
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-        //configurationBuilder.setOAuthConsumerSecret("2lH9gEuUIBkwGoKLXKTKG0kgm4lvLcvKU7xNKoVWZr0").setOAuthConsumerKey("TP7ebHOCMLv9Xd9nkT20Q").setOAuthAccessToken("23996599-7rNHIgpoq7oZSxhplIE7U62qaDtNPBnPFzgRNbBvb").setOAuthAccessTokenSecret("wtDMHs6yfNTxkQnhliQKicoZyB4Uni4J2RJYPFci4LTjt").setHttpRetryCount(0).setHttpConnectionTimeout(5000);
-        configurationBuilder.setOAuthConsumerSecret("zXU9DkEnJyXZYISnfTKMD6jPwzlhqsQfh7DMtBERI").setOAuthConsumerKey("bLvcK5WM1VJyWtJTL5Hpg").setOAuthAccessToken("2814063755-oMeSa2tvOd7gzXnxAbiDqH6HxBRLN9piiPaujCT").setOAuthAccessTokenSecret("Wt1OwiYHiYq7fN8UBSpYw7bapBZMRXa69YwUEZctOnsKE").setHttpRetryCount(0).setHttpConnectionTimeout(5000);
+        configurationBuilder.setOAuthConsumerSecret("").setOAuthConsumerKey("").setOAuthAccessToken("").setOAuthAccessTokenSecret("git ").setHttpRetryCount(0).setHttpConnectionTimeout(5000);
         return new TwitterAdsFactory(configurationBuilder.build()).getAdsInstance();
     }
 
