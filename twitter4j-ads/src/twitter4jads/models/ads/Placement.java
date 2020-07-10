51843e47203c3fc914394ce7c71944d368dd5112
wanqulou <wanqulou@gmail.com>
2020-07-10 11:30:30 +0800

    update placement

--- twitter4j-ads/src/twitter4jads/models/ads/Placement.java
+++ twitter4j-ads/src/twitter4jads/models/ads/Placement.java
@@ -6,22 +6,19 @@ package twitter4jads.models.ads;
  * Time: 1:52 PM
  */
 public enum Placement {
-    // Supported
+
     ALL_ON_TWITTER,
     PUBLISHER_NETWORK,
 
-    // Not supported from v1.0 (Stats)
+    // ALL_ON_TWITTER includes
+    TWITTER_PROFILE,
     TWITTER_SEARCH,
     TWITTER_TIMELINE,
-    TWITTER_PROFILE,
 
-    TAP_MRECT,
-    //TAP
-    TAP_NATIVE,
-    TAP_FULL_LANDSCAPE,
-    TAP_TABLET_LEADERBOARD,
-    TAP_TABLET_FULL,
-    TAP_TABLET_FULL_LANDSCAPE,
+    // PUBLISHER_NETWORK includes
     TAP_BANNER,
-    TAP_FULL
+    TAP_FULL,
+    TAP_FULL_LANDSCAPE,
+    TAP_NATIVE,
+    TAP_MRECT
 }
